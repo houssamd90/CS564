c8c20d17d1be19db6343aa063074860df8cedcfb
Dan Drake <ddrake@epic.com>
2016-12-03 12:32:45 -0600

    date-based plotting test

--- src/StockGUI.java
+++ src/StockGUI.java
@@ -16,12 +16,24 @@ import javax.swing.SwingConstants;
 
 import java.awt.Font;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.util.List;
+import java.util.ArrayList;
+import java.text.SimpleDateFormat; // can remove this after testing?
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.style.markers.SeriesMarkers;
+import org.knowm.xchart.QuickChart;	
+import org.knowm.xchart.SwingWrapper;
 
 public class StockGUI {
 
@@ -310,6 +322,56 @@ public class StockGUI {
 		//Put graphing stuff here. Can get user id from txtUserID and ticker from txtTicker2
 		btnGraph.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				@SuppressWarnings("deprecation")
+				String sql="net_worth_trending(" + txtUserID + ")";
+				try {
+					
+					//ResultSet rs = st.executeQuery(sql);
+					List<java.util.Date> xData = new ArrayList<java.util.Date>();
+					List<Double> yData = new ArrayList<Double>();
+					/*
+					while (rs.next()) {
+						xData.add(rs.getDate("curDate"));
+						yData.add(rs.getFloat("curNetWorth"));						
+					}
+					*/
+
+					DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+					xData.add(sdf.parse("2016-01-01"));
+					xData.add(sdf.parse("2016-02-02"));
+					xData.add(sdf.parse("2016-03-15"));
+					yData.add(1000.0);
+					yData.add(1500.1);
+					yData.add(900.5);
+										
+					//double[] xData = new double[] { 0.0, 1.0, 2.0 };
+				    //double[] yData = new double[] { 2.0, 1.0, 0.0 };
+
+				    // Create Chart
+				    //XYChart chart = QuickChart.getChart("Net worth trend", "Date", "Net worth ($)", "whatevs", xData, yData);
+
+					XYChart chart = new XYChartBuilder().width(800).height(600).title("Net worth trend").build();
+					chart.getStyler().setLegendVisible(false);
+					XYSeries series = chart.addSeries("blah", xData, yData);
+					series.setMarker(SeriesMarkers.NONE);
+					
+				    // Show it
+				    new SwingWrapper(chart).displayChart();
+
+				} catch (Exception er) {
+					er.printStackTrace();
+					return;
+				} finally {
+					if (st != null) {
+						try { 
+							st.close();
+						}
+						catch (SQLException er) {
+							return;
+						}
+					}
+				}
+				
 			}
 		});
 		btnGraph.setBounds(283, 246, 95, 39);
