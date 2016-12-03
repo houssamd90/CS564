cba0b08558c0edebaa9f395edb918cd9e2e4f31a
houssamd90 <houssam.dawood@gmail.com>
2016-12-03 12:15:39 -0600

    s

--- src/StockGUI.java
+++ src/StockGUI.java
@@ -79,7 +79,7 @@ public class StockGUI {
 		//Postgres stuff
 		try{
 			Class.forName("org.postgresql.Driver");
-			Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/classdb","auri","testdb");
+			con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/classdb","auri","testdb");
 			if(con!=null){
 				System.out.println("Connected");
 			}
